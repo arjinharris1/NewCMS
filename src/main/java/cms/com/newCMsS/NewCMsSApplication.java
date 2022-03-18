07036a7e8bef4960844773a7f779bc34824b73da
arjinharris1 <97618413+arjinharris1@users.noreply.github.com>
2022-03-17 23:47:08 -0400

    Add files via upload

---
+++ src/main/java/cms/com/newCMsS/NewCMsSApplication.java
@@ -0,0 +1,13 @@
+package cms.com.newCMsS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCMsSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCMsSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cms/com/newCMsS/NewCMsSApplicationTests.java
@@ -0,0 +1,13 @@
+package cms.com.newCMsS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCMsSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
